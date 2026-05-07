abc03f9b8dd1edc1c55f0a12c712ddcb756f2232
Kayla Iacovino <captainkayla@gmail.com>
2026-05-07 15:13:26 -0500

    setup stubs for plotting funcs

---
+++ stellar_geology/plot/__init__.py
@@ -0,0 +1,13 @@
+"""
+Stellar Geology plotting submodule
+
+Functions for creating ternary and other useful plots related to stellar geology calculations
+
+This is a stub.
+"""
+
+from ternary_plot import *
+
+__all__ = [
+    'ternary_plot',
+]
