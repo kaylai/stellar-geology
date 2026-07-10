411e39c338a5b474733579f0945cf0e1f3a58ba5
Kayla Iacovino <captainkayla@gmail.com>
2026-07-09 21:28:16 -0500

    Bump to version 0.1.0. Make pyproject.toml single source of truth for version number.

--- stellar_geology/__init__.py
+++ stellar_geology/__init__.py
@@ -4,14 +4,16 @@ Stellar Geology
 A python library for calculating planet geochemistry from stellar compositions.
 """
 
-__version__ = "0.0.1"
-__author__ = "Kayla Iacovino"
+from importlib.metadata import version as _version
 
 from .star import Star
 from .planet import Planet
 from .constants import *
 from .mineralogy import calculate_mineralogy, calculate_composition_from_mineralogy, plot_norm
 from .plot import ternary_plot
+
+__version__ = _version("stellar_geology")
+__author__ = "Kayla Iacovino"
 
 __all__ = [
     # Classes
